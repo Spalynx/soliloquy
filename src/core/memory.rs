ff018e920b449e9535ee65c3eb093329c021d8ed
Spalynx <Spalynx@gmail.com>
2018-05-27 11:14:35 -0500

    First pomodoro of the day. Read up about memory, and the program counter. MEM -> Added a basic u8 array for handling internal ram. Reading around, it seems that the other ~60,000 memory locations will have to be handled in separate modules. Therefore, the memory module will call each module based on the address requested.

--- src/core/memory.rs
+++ src/core/memory.rs
@@ -1,3 +1,44 @@
+//For now, memory is a 2kb sized 8 bit array.
+//This might not ever change, but it seems a tad too simplistic.
+pub struct MEM {
+    values:	[u8, 0x800], //2kb internal RAM.
+}
+
+#[allow(dead_code)]
+impl memory {
+//Initializes an empty memory struct.
+    pub fn new() -> MEM {
+        MEM {
+            values:	[u8, 0x800],
+            //Should be things like PPU memory, etc.
+        }
+    }
+
+    //Obtains values from full memory map.
+    pub fn get(&self, address: u16) -> u8 {
+        if(address >= 0x00 && address <= 0x800){
+            //2kb internal ram
+            self.values[address]
+        }
+        else {
+            panic!("Other values in the memory map not implemented yet!");
+        }
+    }
+
+    //Stores a value into memory. The conditional statements should
+    // block any illegal storing.
+    pub fn set(&mut self, address: u16, val: u8){
+        if(address >= 0x00 && address <= 0x800){
+            //2kb internal ram
+            self.values[address] = val;
+        }
+        else {
+            panic!("Other values in the memory map not implemented yet!");
+        }
+    }
+}
+
+
 /*Memory locations $200 to $5ff map to the screen pixels. Different values will
 draw different colour pixels. The colours are:
 
@@ -17,4 +58,6 @@ $c: Grey
 $d: Light green
 $e: Light blue
 $f: Light grey
+
+I think this might be easy 6502 specific...
 */
