10e39fc3a6e488b031137164942f44dc50b677a8
Spalynx <Spalynx@gmail.com>
2018-06-17 12:52:19 -0500

    Move CPU tests to cpu_test.rs, prettify CPU code
    It's been a chore to constantly jump 400+ lines just to check something I wrote
    in the implementation. Sure, this is simplified by vim marks, but extremely
    disorienting. It seems to be a common trend for people to be thwarted by tests
    that are as long (if not longer than production code), therefore, I'm now
    shoving all of it in their own files.
    
    Using google coding styles, all test files will be denoted by *_test.rs.
    
    CPU
     * Generally prettified doc comments.
     * Removed test mod, no need to restate above.

--- src/core/cpu.rs
+++ src/core/cpu.rs
@@ -2,17 +2,22 @@
  *  Author: Spalynx
  *  Init: 12/8/17
  */
-
 use core::memory::MEM;
-#[macro_use]
+
+//Test module definition.
+    #[cfg(test)]
+    #[path = "./cpu_test.rs"]
+    pub mod cpu_test;
+
 
 //General helper macro, might move to a general module.
-macro_rules! bytes_to_word {
-    ($h:expr,$l:expr) => (($h << 8) | ($l & 0xff));
-}
+    #[macro_use]
+    macro_rules! bytes_to_word {
+        ($h:expr,$l:expr) => (($h << 8) | ($l & 0xff));
+    }
 
-//CPU=DEFINITION====================================================================================
-//==================================================================================================
+//CPU=DEFINITION================================================================
+//==============================================================================
 
 /// The MOS Technology 6052 CPU of the NES.
 ///
@@ -30,28 +35,26 @@ macro_rules! bytes_to_word {
 /// it's instruction set, and it commands to parse instructinos.
 pub struct CPU {
     pub memory:         MEM,
-    pub cycles:         u64,    //Clock cycle counter. Other hardware relies on this. [5]
-    pub pc:             u16,    //Program Counter - Supports 65536 direct memory locations.
-    pub sp:             u8,     //Stack Pointer - Accessed using interrupts, pulls, pushes,
-    // and transfers.
+    pub cycles:         u64,    //Clock cycle counter.
+                                //      Other hardware relies on this. [5]
+    pub pc:             u16,    //Program Counter - 65536 memory locations.
+    pub sp:             u8,     //Stack Pointer - Accessed using interrupts,
+                                //     pulls, pushes, and transfers.
 
-    pub a:              u8,     //Accumulator
-    pub x:              u8,     // x register
-    pub y:              u8,     // y register
+    pub a:              u8,     // Accumulator.
+    pub x:              u8,     // X register.
+    pub y:              u8,     // Y register.
 
-    pub status:          u8,     //CPU Flags See [1] for reference
+    pub status:          u8,    // CPU Flags See [1] for reference
 
-    pub interrupt:      u8,     // interrupt type to perform
-    pub stall:          u8,    // number of cycles to stall
+    pub interrupt:      u8,     // Interrupt type to perform.
+    pub stall:          u8,     // Number of cycles to stall.
 }
 
 
-//~CPU-IMPLEMENTATION================================================================================
-//===================================================================================================
+//~CPU-IMPLEMENTATION===========================================================
+//==============================================================================
 
-/// There will be two main function types here:
-///      CPU instructions     - OPCODES
-///      CPU meta functions   - get_status, parse_opcodes.
 #[allow(dead_code)]
 impl CPU {
     /// Initializes an empty CPU struct
@@ -101,7 +104,6 @@ impl CPU {
 
     }
 
-    /// throw_interrupt
     /// Receives a string as a param, and throws one of the 3 (?) cpu
     /// interrupts.
     /// NOTE: This is me pushing off interrupts until I actually
@@ -207,8 +209,9 @@ impl CPU {
         }
     }
 }
+/*
     //Folded opcodes
-    /*
+
     /// ADC
     /// ADd with Carry. ADC results are based on the decimal flag. In
     /// decimal mode, addition is carried out as if the values are in Binary
@@ -287,12 +290,6 @@ impl CPU {
     pub fn BVS(&self) {}
 
         
-
-
-
-        
-
-
     ///CMP (CoMPare accumulator) 
     ///Affects Flags: S Z C 
     ///+ add 1 cycle if page boundary crossed
@@ -372,8 +369,8 @@ impl CPU {
     */
 
 
-//=ADDRESSING-MODES=================================================================================
-//==================================================================================================
+//=ADDRESSING-MODES=============================================================
+//==============================================================================
 /// AddressingMode trait for opcodes within the CPU.
 ///
 /// I think that the hardest part in the conceptualization of AM
@@ -395,7 +392,6 @@ impl CPU {
 /// This struct emulates the NES cpu.
 /// CPU holds within it: a set of registers, a connection to memory,
 /// it's instruction set, and it commands to parse instructinos.
-/// TODO: UNTESTED
 trait AddressingMode {
     fn load (&self, cpu: &mut CPU) -> u8;
     fn save (&self, cpu: &mut CPU, storeval: u8);
@@ -492,14 +488,15 @@ impl AddressingMode for IndirectIndexedAM {
 	let high = cpu.memory.get_zp( self.address );
         let low = cpu.memory.get_zp( self.address + 1);
 
-        cpu.memory.set( bytes_to_word!(high as u16,low as u16) + cpu.y as u16, storeval );
+        cpu.memory.set( bytes_to_word!(high as u16,low as u16) + cpu.y as u16,
+                        storeval );
     }
 }
 
 
 
-//=CPU-INSTRUCTION-INFO-TABLES======================================================================
-//==================================================================================================
+//=CPU-INSTRUCTION-INFO-TABLES==================================================
+//==============================================================================
 /// Contructs tables for all cpu instructions on all addressing modes.
 ///
 /// When tables are initialized with fn new_instructions(), there are 256
@@ -508,22 +505,15 @@ impl AddressingMode for IndirectIndexedAM {
 /// 6 tables exist: instruction names, instruction sizes, instruction modes,
 /// instruction speeds, and instruction speeds when a page boundary is crossed.
 pub struct Instructions {
-    pub names:		[&'static str;256],
-    pub sizes:		[u8;256],
-    pub modes:		[u8;256],
-    pub speeds:		[u8;256],
-    pub paging:	  [u8;256],
+    pub names:     [&'static str;256],
+    pub sizes:     [u8;256],
+    pub modes:     [u8;256],
+    pub speeds:    [u8;256],
+    pub paging:    [u8;256],
 }
 
-// TODO: Move impl to definition, once project is runnable again.
-    
 /// Initializes struct instruction with all possible 6502 instructions.
-///
-/// i'm not sure how much memory this process takes, but efforts were made
-/// to use values of lower sizes.
-///
-/// see [6502 unofficial opcodes](https://wiki.nesdev.com/w/index.php/cpu_unofficial_opcodes)
-/// for the full table and info.
+/// I would like to make this immutable, or something...
 impl Instructions {
     pub fn new() -> Instructions {
         Instructions {
@@ -599,249 +589,24 @@ impl Instructions {
                 3, 3, 5, 5, 0, 2, 0, 2, 4, 4, 6, 6, 3, 6, 0, 8, 4, 4, 6, 6, 0, 5, 0,
                 7, 5, 5, 7, 7],
         }
-	}
+    }
 }
 
 
+/* Temporary dead code.
 
-
+macro_rules! vec_of_strings {
+($($x:expr),*) => (vec![$($x.to_string()),*]);
+}
 
 
 #[allow(dead_code)]
 /// Emulates a CPU step.
-///
-/// #Examples
-/// ```
-//
-/// ```
 /// Contains fields for an address, programcounter, and cpu mode.
 pub struct CpuStep {
 	  address:    u16,
 	  pc:         u16, 
 	  mode:       u8, 
 }
-//type cpuop = fn(CpuStep) -> u8;
-
-
-
-//=-TESTING-========================================================================================
-//==================================================================================================
-#[cfg(test)]
-pub mod tests {
-    //Tests could be greatly improved with
-    // [before_each]/[assemblyintialize]... Maybe one day!
-    use super::*;
-
-    #[test]
-    fn test_cpu_init(){
-        let test_cpu = super::CPU::new();
-        
-        //Testing each struct field initialization.
-        assert_eq!(test_cpu.cycles,   0);
-        assert_eq!(test_cpu.pc,       0);
-        assert_eq!(test_cpu.sp,       0);
-        assert_eq!(test_cpu.x,        0);
-        assert_eq!(test_cpu.y,        0);
-        assert_eq!(test_cpu.status,   0);
-        assert_eq!(test_cpu.interrupt,0);
-        assert_eq!(test_cpu.stall,    0);
-    }
-
-    #[test]
-    fn test_instruction_init(){
-        let test_instr = super::Instructions::new();
-
-        //Testing the instruction table for init.
-        assert_eq!(test_instr.names.len()     ,256);
-        assert_eq!(test_instr.sizes.len()     ,256);
-        assert_eq!(test_instr.modes.len()     ,256);
-        assert_eq!(test_instr.speeds.len()    ,256);
-        assert_eq!(test_instr.paging.len()    ,256);
-
-        //This can be expanded later when testing opcode parsing.
-    }
-
-    #[test]
-    fn test_status_each(){
-        let mut cpu = super::CPU::new();
-
-        //Array of status codes.
-        let status = ["N", "n", "V", "v", "S", "s", "B", "b", "D", "d", "I", "i", "Z", "z", "C", "c"];
-
-        //Loops through array of status codes, and sets/resets each flag.
-        for f in status.iter() {
-            //Setting each flag.
-            cpu.set_status(f, true);
-            assert_eq!(cpu.get_status(f), true);
-
-            //Resetting each flag.
-            cpu.set_status(f, false);
-            assert_eq!(cpu.get_status(f), false);
-        }
-
-    }
-
-    #[test]
-    fn test_status_settingtwice(){
-        //Making sure that nothing is changed when setting something twice.
-        // When I would set a false flag to false, it would be set to true.
-        let mut cpu = super::CPU::new();
-
-        //False
-        cpu.set_status("I", false);
-        cpu.set_status("I", false);
-        assert_eq!(cpu.get_status("I"), false);
-        
-        //True
-        cpu.set_status("V", true);
-        cpu.set_status("V", true);
-        assert_eq!(cpu.get_status("V"), true);
-    }
-
-    #[test]
-    fn test_new_memory(){
-        let mut cpu = super::CPU::new();
-        let mem = super::MEM::new();
-
-        cpu.new_memory(mem);
-
-        //Testing access/mutability.
-        cpu.memory.set_zp(0x1A, 10);
-        assert_eq!(cpu.memory.get_zp(0x1A), 10);
-        cpu.memory.set_zp(0x0, 254);
-        assert_eq!(cpu.memory.get_zp(0x0), 254);
-        cpu.memory.set(0x800-1, 111);
-        assert_eq!(cpu.memory.get(0x800-1), 111);
-
-        //Further mem testing should be done in it's module.
-    }
-
-    #[test]
-    fn testOP_flags(){
-        //Testing CLC, CLD, CLI, CLV, SEC, SED, SEI.
-        // Reliant on test_status() being correct.
-        //Yes, I understand that I should split this up,
-        // but cargo test doesn't really avoid bpcode atm.
-
-        let mut cpu = super::CPU::new();
-
-        //~! CLEARS
-
-        //Carry clear.
-        cpu.set_status("C", true);
-        cpu.CLC();
-        assert_eq!(cpu.get_status("C"), false);
-        //Decimal clear.
-        cpu.set_status("D", true);
-        cpu.CLD();
-        assert_eq!(cpu.get_status("D"), false);
-        //Interrupt carry.
-        cpu.set_status("I", true);
-        cpu.CLI();
-        assert_eq!(cpu.get_status("I"), false);
-
-        //~! SETS
-        //Carry set.
-        cpu.set_status("C", false);
-        cpu.SEC();
-        assert_eq!(cpu.get_status("C"), true);
-        //Decimal set.
-        cpu.set_status("D", false);
-        cpu.SED();
-        assert_eq!(cpu.get_status("D"), true);
-        //Interrupt set.
-        cpu.set_status("I", false);
-        cpu.SEI();
-        assert_eq!(cpu.get_status("I"), true);
-        
-
-    }
-
-    #[test]
-    fn testOP_lda(){
-        /// self.lda(ZeroPageAM{0x2B});
-        let mut cpu = super::CPU::new();
-
-        cpu.memory.set(0xAF, 123);  // Random position in zero page.
-        cpu.memory.set(0x755, 456); // Random position non zero page.
-
-        //The rest should be mechanically similar to these 3, and tested in test_AM.
-        //assert_eq!(cpu.LDA(ImmediateAM{address: 10}), cpu.a, "AM: Immediate.");
-        cpu.LDA(AbsoluteAM{address:  0x755});
-        assert_eq!(456, cpu.a, "AM: Absolute");
-        cpu.LDA(ZeroPageAM{address:  0xAF});
-        assert_eq!(123, cpu.a, "AM: Absolute");
-
-        //Testing flag changes.
-        cpu.LDA(ImmediateAM{address: 0});
-        assert_eq!(cpu.get_status("Z"), true, "Testing zero.");
-        cpu.LDA(ImmediateAM{address: 243});
-        assert_eq!(cpu.get_status("N"), true, "Testing negative.");
-    }
-
-    /*
-    #[test]
-    fn testOP_adc(){
-        //Testing for basic ADd with Carry.
-        let test_cpu = super::CPU::new();
-
-        test_cpu.LDA(254);
-        test_cpu.ADC(6);
-        assert_eq!(test_cpu.A, 5);
-        assert_eq!(test_cpu.get_status("C"), 1);
-    }
-    #[test]
-    fn testOP_adc_signed (){
-        //Testing signed arithmetic.
-        let test_cpu = super::CPU::new();
-
-        test_cpu.LDA(0b01111111u8); //+127
-        test_cpu.ADC(0b00000010u8); //+2
-        assert_eq!(test_cpu.A, 0b10000001u8); // = -127
-        //Overflow should be set because bit 7 is '1'.
-        assert_eq!(test_cpu.get_status("O"), 1);
-    }
-    #[test]
-    fn testOP_adc_decimal(){
-        //TODO: Test adc with a decimal.
-        let test_cpu = super::CPU::new();
-
-        test_cpu.SED();
-        assert!();
-    }
-    #[test]
-    fn testOP_flags(){
-        /// TEST -> CLC, CLD, CLI, CLV, SEC, SED, SEI
-        let test_cpu = super::CPU::new();
-    }
-    */
-
-}
-
-
-//[1]	0: carry flag
-//	1: zero flag
-//  	2: interrupt disable flag
-//  	3: decimal mode flag
-//  	4: break command flag
-//  	5: unused flag
-//  	6: overflow flag
-//  	7: negative flag
-//
-//[3] 	izx = indexed indirect =  12
-//    	izy = indirect indexed = 11
-//    	ind = indirect = 10
-//    	zp = zero page = 3
-//    	zpx = zero page x = 4
-//    	zpy = zero page y = 5
-//    	abs = absolute val = 6
-//    	abx = absolute val x = 7 
-//    	aby = absolute val y = 8
-//    	imm = immediate = 2
-//    	rel = relative = 9
-
-/*
-macro_rules! vec_of_strings {
-($($x:expr),*) => (vec![$($x.to_string()),*]);
-}
+type cpuop = fn(CpuStep) -> u8;
 */
