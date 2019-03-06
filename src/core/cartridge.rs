f163a2af61496179f4c39408c1afcd6be3e98513
Spalynx <Spalynx@gmail.com>
2019-03-05 23:52:44 -0600

    Implement parse_opcode() for official instructions
    Also added some pseudocode in the cpu.step() funciton.
    Not sure how Rust will allow passing of a file buffer, but that's the
    next hurdle. Considering that EVERYTHING involving files is unsafe,
    and I'd like to keep my code in the safe zone, I might have to
    outsource all stepping to the nes.rs structure.
    This would negatively affect future changes, as far as I can tell.

--- src/core/cartridge.rs
+++ src/core/cartridge.rs
@@ -1,9 +1,16 @@
-pub struct ROM {
+extern crate memmap;
 
+use self::memmap::Mmap;
+use std::fs::File;
+
+pub struct ROM {
+    filename: String, 
 }
 
 impl ROM {
-    pub fn new() -> ROM {
-    	ROM{}
+    pub fn new(file_n: String) -> ROM {
+    	ROM{
+            filename:   file_n,
+        }
     }
 }
